c1c8b8e223b023a21dc432a23c15a78d79f4917e
Dipongkar Roy <dipongkarroy233@gmail.com>
2024-02-24 16:59:36 +0600

    project done

---
+++ src/components/Marquree.jsx
@@ -0,0 +1,13 @@
+
+const Marquree = ({ rev }) => {
+    const { name, details, rating } = rev;
+    return (
+        <div className="border-2 border-green-400 w-80 h-64 pt-5 px-4 ">
+        <h2 className="text-center text-xl font-mono">{name}</h2>
+        <p>{details}</p>
+        <p className="text-center">{rating}</p>
+    </div>
+    );
+};
+
+export default Marquree;
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Marquree from "./Marquree";
+import Marquee from "react-fast-marquee";
+
 
 const Testimonial = () => {
+    const [reviews ,setReviews] = useState([])
+
+    useEffect(()=>{
+        fetch('review.json')
+         .then(res=>res.json())
+         .then(data =>setReviews(data))
+    },[])
     return (
         <>
             <div className="text-center mt-20">
@@ -7,6 +19,17 @@ const Testimonial = () => {
                 <h2 className="text-4xl font-semibold">What Customer Says</h2>
                 <div className="text-1xl px-4 mt-3"> the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</div>
             </div>
+            
+            <Marquee className="mt-6 gap-4">
+               {
+               reviews.map(rev=><Marquree
+               key={rev._id}
+               rev={rev}
+               ></Marquree>)
+              
+               }
+            </Marquee>
+            
         </>
     );
 };
